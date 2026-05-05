cef07d245c952524a6860eda790f4cdfe46c1384
Teknium <127238744+teknium1@users.noreply.github.com>
2026-05-05 04:22:59 -0700

    chore: AUTHOR_MAP entry for chengoak

--- scripts/release.py
+++ scripts/release.py
@@ -56,6 +56,7 @@ AUTHOR_MAP = {
     "657290301@qq.com": "IMHaoyan",
     "revar@users.noreply.github.com": "revaraver",
     "29756950+revaraver@users.noreply.github.com": "revaraver",
+    "chengoak@users.noreply.github.com": "chengoak",
     "emelyanenko.kirill@gmail.com": "EmelyanenkoK",
     # Matrix parity salvage batch (April 2026)
     "sr@samirusani": "samrusani",
